0db15ac35e928dc4149392b0da3703fe0b4875fc
gizzhung <mayday0140@gmail.com>
2021-04-12 16:35:49 +0800

    nuxt/vuetify test  4/12

---
+++ pages/inspire.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container >
+    <v-row 
+      no-gutters
+      v-for="r in 6"
+      :key="r"
+    >
+      <v-col
+        v-for="n in 4"
+        :key="n"
+        cols="12"
+        sm="3"
+      >
+      <v-card
+        class="pa-2"
+        outlined
+        tile
+      >
+     <card></card>
+      </v-card>
+
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import card from '@/components/card.vue'
+export default {
+  
+}
+</script>
